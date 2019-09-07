58b0c9eb074912da34d07bd5304289aef01acba0
Payam <thisispayam@gmail.com>
2019-09-06 19:24:05 -0700

    update contact on server

--- client/src/context/contact/contactState.js
+++ client/src/context/contact/contactState.js
@@ -81,6 +81,30 @@ const ContactState = props => {
         }
        
     }
+
+    //Update Contact
+    const updateContact = async contact => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        try {
+            const res = await axios.put(`/api/contacts/${contact._id}`, contact, config);
+
+            dispatch({
+                type: UPDATE_CONTACT,
+                payload: res.data
+            });
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg
+            });
+        }
+    }
+
     //Clear Contact
     const clearContacts = () => {
         dispatch({ type: CLEAR_CONTACTS })
@@ -92,10 +116,6 @@ const ContactState = props => {
     //Clear Current Contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT })
-    }
-    //Update Contact
-    const updateContact = contact => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact })
     }
     //Filter Contacts
     const filterContact = text => {
